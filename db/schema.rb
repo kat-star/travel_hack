0ccab91d67721f31b0357464d11cfd96d1f96e0b
kat-star <52055037+kat-star@users.noreply.github.com>
2020-05-23 15:20:14 -0700

    update database to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2019_10_23_205245) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id", null: false
-    t.integer "hack_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "hack_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["hack_id"], name: "index_comments_on_hack_id"
@@ -42,8 +45,8 @@ ActiveRecord::Schema.define(version: 2019_10_23_205245) do
     t.string "category"
     t.string "title"
     t.text "content"
-    t.integer "user_id", null: false
-    t.integer "destination_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "destination_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["destination_id"], name: "index_hacks_on_destination_id"
@@ -51,8 +54,8 @@ ActiveRecord::Schema.define(version: 2019_10_23_205245) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "hack_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "hack_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["hack_id"], name: "index_likes_on_hack_id"
